e06fc145443aa79037a42371985f50f8fc5c6dfd
Vu Hung <phamvuhung2710@gmail.com>
2023-03-15 20:36:53 +0700

    organize folder structure, reset css, custom theme mu

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme'
+import { CssBaseline } from '@mui/material'
+import Topbar from './components/Topbar'
+import Sidebar from './components/Sidebar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="app">
+        <main className="content">
+          <Topbar />
+          <Sidebar />
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
+    </ThemeProvider>
   )
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,42 @@
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2196f3'
+    },
+    secondary: {
+      main: '#f50057'
+    }
+  },
+  typography: {
+    fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
+    fontSize: 12,
+    h1: {
+      fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
+      fontSize: 40
+    },
+    h2: {
+      fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
+      fontSize: 32
+    },
+    h3: {
+      fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
+      fontSize: 24
+    },
+    h4: {
+      fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
+      fontSize: 20
+    },
+    h5: {
+      fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
+      fontSize: 16
+    },
+    h6: {
+      fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
+      fontSize: 14
+    }
+  }
+})
+
+export default theme
